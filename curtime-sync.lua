ce7918fe4dab3460ff92b48439fd21d66913bb56
RaphaelIT7 <raphael.i.t@outlook.de>
2022-12-12 19:54:02 +0100

    [CurTime-Sync] Added File

---
+++ curtime-sync.lua
@@ -0,0 +1,19 @@
+if SERVER then
+	hook.Add("Think", "Engine_TickCount", function()
+		SetGlobal2Int("engine_tickcount", engine.TickCount())
+	end)
+
+	function CurTime()
+		return engine.TickCount() / math.floor(1 / engine.TickInterval())
+	end
+else
+	engine.OldTickCount = engine.TickCount
+	function engine.TickCount()
+		return GetGlobal2Int("engine_tickcount", 0)
+	end
+
+	OldCurTime = CurTime
+	function CurTime()
+		return engine.TickCount() / math.floor(1 / engine.TickInterval())
+	end
+end

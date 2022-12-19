c1a0d0d49bef93288b7cc8295cbe67455ff986a9
RaphaelIT7 <raphael.i.t@outlook.de>
2022-12-19 17:33:54 +0100

    [CurTime-Sync] Reworked IT
    [+] It now Syncs every Second
    [+] no more laggy CurTime
    [+] It can use the net libary if wanted

--- curtime-sync.lua
+++ curtime-sync.lua
@@ -3,22 +3,55 @@
 
 	https://github.com/Facepunch/garrysmod-issues/issues/2502
 ]]
-if SERVER then
-	hook.Add("Think", "Engine_TickCount", function()
-		SetGlobal2Int("engine_tickcount", engine.TickCount())
-	end)
 
-	function CurTime()
-		return engine.TickCount() / math.floor(1 / engine.TickInterval())
-	end
-else
-	engine.OldTickCount = engine.TickCount
-	function engine.TickCount()
-		return GetGlobal2Int("engine_tickcount", 0)
-	end
+local SyncDelay = 1 -- in seconds
+local SyncType = "net" -- can be NW2 or net
 
-	OldCurTime = CurTime
-	function CurTime()
-		return engine.TickCount() / math.floor(1 / engine.TickInterval())
+if SyncType == "NW2" then
+	--[[
+		NW2 solution
+	]]
+	if SERVER then
+		timer.Create("CurTime-Sync", SyncDelay, -1, function()
+			Entity(0):SetNW2Float("CurTime-Sync", CurTime()) -- NW2Float has no precision error unlike NWFloat. Functions the same as SetGlobal2Float.
+		end)
+	else
+		hook.Add("InitPostEntity", "CurTime-Sync", function()
+			local SyncTime = 0
+			Entity(0):SetNW2VarProxy("CurTime-Sync", function(_, _, _, ServerCurTime)
+				SyncTime = OldCurTime() - ServerCurTime
+			end)
+
+			OldCurTime = OldCurTime or CurTime -- added if you should ever need it. just add local if you dont.
+			function CurTime()
+				return OldCurTime() - SyncTime
+			end
+		end)
+	end
+elseif SyncType == "net" then
+	--[[
+		net solution
+	]]
+	if SERVER then
+		util.AddNetworkString("CurTime-Sync")
+		timer.Create("CurTime-Sync", SyncDelay, -1, function()
+			net.Start("CurTime-Sync", true) -- Can be skipped because It is not needed to sync every second.
+				net.WriteFloat(CurTime())
+			net.Broadcast()
+		end)
+	else
+		hook.Add("InitPostEntity", "CurTime-Sync", function()
+			local SyncTime = 0
+			net.Receive("CurTime-Sync", function()
+				local ServerCurTime = net.ReadFloat()
+				if !ServerCurTime then return end
+				SyncTime = OldCurTime() - ServerCurTime
+			end)
+
+			OldCurTime = OldCurTime or CurTime -- added if you should ever need it. just add local if you dont.
+			function CurTime()
+				return OldCurTime() - SyncTime
+			end
+		end)
 	end
 end

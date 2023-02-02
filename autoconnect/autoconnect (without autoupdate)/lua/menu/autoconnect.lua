4e0063512bc419f8da529277bc241f5c75e9e566
RaphaelIT7 <raphael.i.t@outlook.de>
2023-02-02 20:54:51 +0100

    [AutoConnect] Update. Hopefully fixed Errors

--- autoconnect/autoconnect (without autoupdate)/lua/menu/autoconnect.lua
+++ autoconnect/autoconnect (without autoupdate)/lua/menu/autoconnect.lua
@@ -23,6 +23,11 @@
 
 	Created by: Raphael(https://github.com/RaphaelIT7)
 ]]
+local noaddons, noworkshop = GetAddonStatus()
+if noaddons then return end
+
+
+local cancel = false
 local lastip = ""
 local retrys = 0
 local retrydelay = 0 -- additional retry delay if youre too fast for the server.
@@ -39,8 +44,8 @@ function PingServer_Retry(address, bool)
 		print("[Autoconnect] failed to connect to server. Retrying")
 	end
 
-	if input.IsKeyDown(KEY_ESCAPE) or input.WasKeyPressed(KEY_ESCAPE) then
-		print("[Autoconnect] Retry canceled by user.")
+	if cancel then
+		cancel = false
 		return
 	end
 
@@ -49,9 +54,16 @@ function PingServer_Retry(address, bool)
 	end)
 end
 
+local failed_pings = 0
 function PingServer_CallBack(ping, name, desc, map, players, maxplayers, botplayers, pass, lastplayed, address, gamemode, workshopid, isanon, version, localization, gmcategory)
 	retrys = retrys + 1
+	print("[Autoconnect] Got Ping.")
 	if !ping then
+		failed_pings = failed_pings + 1
+		print("[Autoconnect] Failed Pings: " .. failed_pings)
+		PingServer_Retry(address)
+
+		if true then return end
 		print("[Autoconnect] Reached Ping limit or Server shutdown. Waiting 5 Seconds.")
 		print("[Autoconnect] failed to connect to server. Slowing retry speed from " .. math.Round(retryspeed:GetFloat(), 2) + retrydelay .. " to " .. math.Round(retryspeed:GetFloat(), 2) + retrydelay + 0.1)
 		retrydelay = retrydelay + 0.1
@@ -88,9 +100,9 @@ function PingServer_CallBack(ping, name, desc, map, players, maxplayers, botplay
 end
 
 concommand.Add("autoconnect", function(_, _, args)
-	print(args[1], args[2], args[3], args[4])
-	print(args[2] == ":", isnumber(args[3]))
-	print(type(args[3]))
+	//print(args[1], args[2], args[3], args[4]) Debug prints
+	//print(args[2] == ":", isnumber(args[3]))
+	//print(type(args[3]))
 	if args[2] == ":" then
 		args[1] = args[1] .. args[2] .. args[3]
 		args[3] = nil
@@ -126,6 +138,15 @@ concommand.Add("autoconnect", function(_, _, args)
 
 		retrys = 0
 		retrydelay = 0
+		failed_pings = 0
+
+		hook.Add("Think", "Autoconnect_cancel", function()
+			if input.IsKeyDown(KEY_ESCAPE) or input.WasKeyPressed(KEY_ESCAPE) then
+				print("[Autoconnect] Retry canceled by user.")
+				cancel = true
+				hook.Remove("Think", "Autoconnect_cancel")
+			end
+		end)
 
 		timer.Simple(5, function()
 			print("[Autoconnect] Connecting to server")

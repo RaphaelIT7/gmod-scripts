483c673368c7dc10c468c23a792953d968f33c8e
RaphaelIT7 <raphael.i.t@outlook.de>
2024-01-29 04:51:37 +0100

    [Game-IsDedicated-Sync] Deleted it. (It got fixed)

--- game-dedicated-sync/README.md
+++
@@ -1,4 +0,0 @@
-# game-dedicated-sync
-This script solves the game.IsDedicated always returning false on the client bug.
-
-https://github.com/Facepunch/garrysmod-issues/issues/1495
--- game-dedicated-sync/game-dedicated-sync/lua/autorun/sh_game-dedicated-sync.lua
+++
@@ -1,8 +0,0 @@
-if SERVER then
-	SetGlobal2Bool("IsDedicated", game.IsDedicated())
-else
-	game.OldIsDedicated = game.OldIsDedicated or game.IsDedicated
-	function game.IsDedicated()
-		return GetGlobal2Bool("IsDedicated")
-	end
-end
